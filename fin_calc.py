d093be71635b8361d38ffa90999efcf68200d0af
tobyxiv <50784596+tobyxiv@users.noreply.github.com>
2019-12-12 18:26:37 -0800

    Add files via upload

---
+++ fin_calc.py
@@ -0,0 +1,97 @@
+from flask import Flask,request
+
+from html_calc import calc_splashHTML, pv_bondHTML, pv_bond_resultHTML
+from html_calc import perpHTML, perp_resultHTML
+from html_calc import mortHTML, mort_resultHTML
+from html_calc import errorMessageHTML
+
+from html_calc import homeHTML
+
+import numpy as np
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return homeHTML
+
+@app.route('/calc_home')
+def calculator_landing_page():
+    return calc_splashHTML
+
+@app.route('/pv_bond')
+def pv_bond():
+    return pv_bondHTML
+
+@app.route('/pv_bond_result')
+def pv_bond_result():
+    try: 
+        irate = float(request.args.get('irate'))/100
+        comp_fq = float(request.args.get('comp_fq'))
+        p_cf = float(request.args.get('p_cf'))
+        f_v = float(request.args.get('f_v'))
+        n_y = float(request.args.get('n_y'))
+        pv_name = np.pv(irate/comp_fq, comp_fq*n_y, p_cf, f_v, when='end')
+        return pv_bond_resultHTML.format(irate_field=irate,comp_fq_field=comp_fq, p_cf_field=p_cf, f_v_field=f_v, n_y_field = n_y, pv_field = pv_name)
+
+    except ZeroDivisionError:
+        return errorMessageHTML.format(error_field = 'You may not have an interest rate of zero.')
+    except ValueError:
+        return errorMessageHTML.format(error_field = 'Please enter integers.')
+    except:
+        return errorMessageHTML.format(error_field = 'Something went wrong. Try entering the most logical integer values.')
+
+@app.route('/perp')
+def perp():
+    return perpHTML
+
+@app.route('/perp_result')
+def perp_result():
+    try:
+        irate = float(request.args.get('irate'))/100
+        p_cf = float(request.args.get('p_cf'))
+        pv_name = p_cf/irate
+        return perp_resultHTML.format(irate_field=irate, p_cf_field=p_cf, pv_field=pv_name)
+
+    except ZeroDivisionError:
+        return errorMessageHTML.format(error_field = 'You may not have an interest rate of zero.')
+    except ValueError:
+        return errorMessageHTML.format(error_field = 'Please enter integers.')
+    except:
+        return errorMessageHTML.format(error_field = 'Something went wrong. Try entering the most logical integer values.')
+
+@app.route('/mort')
+def mort():
+    return mortHTML
+
+@app.route('/mort_result')
+def mort_result():
+    try: 
+        loan = float(request.args.get('loan'))
+        irate = float(request.args.get('irate'))/100
+        loan_term = float(request.args.get('loan_term'))
+        
+        a_irate = irate/12
+        a_loan_term = loan_term*12
+        num = a_irate*((1+a_irate)**(a_loan_term))
+        denom = ((1+a_irate)**(a_loan_term))-1
+        
+        mo_pmt = loan*num/denom
+        
+        return mort_resultHTML.format(loan_field=loan, irate_field=irate, loan_term_field=loan_term, mo_pmt_field = mo_pmt)
+
+    except ZeroDivisionError:
+        return errorMessageHTML.format(error_field = 'You may not have an interest rate of zero.')
+    except ValueError:
+        return errorMessageHTML.format(error_field = 'Please enter integers.')
+    except:
+        return errorMessageHTML.format(error_field = 'Something went wrong. Try entering the most logical integer values.')
+
+@app.route('/error')
+def error():
+    return errorMessageHTML
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ html_calc.py
@@ -0,0 +1,259 @@
+# Home page
+homeHTML = """
+<!-- OMIS30 HTML Template-->
+<style>body{background-color: #ffb366;}</style>
+<p><br /><center><h1>Financial Calculator</h1></p>
+<p><center><h3>Created By: Patrick Mahan, Dylan Wirawan, Gabriel Singer, Aidan Mcerlean</h3></p>
+<p><center><img src="https://i.postimg.cc/65xHD96K/imageedit-2-6626502388.png" style="width:300px;height:400px;"></p>
+<p><button type="button" class="block" style="display: block; width: 100%; border: none; background-color: #4CAF50; padding: 14px 28px; font-size: 16px; cursor: pointer; text-align: center;"><a href="calc_home">Enter</button></a></p>
+""".strip()
+
+calc_splashHTML = """
+<!-- OMIS30 HTML Template-->
+"""
+# Calculators page
+calc_splashHTML = """
+<!-- OMIS30 HTML Template-->
+<style>body{background-color: #ffb366;}</style>
+<p>&nbsp;</p>
+<p><center><h3>Choose what you would like to calculate below:</p>
+<p>&nbsp;</p>
+<center><button type="button" style="background-color: #4CAF50; border: none; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;"><a href="/pv_bond">Present Value of a Bond</button></a>
+<p>&nbsp;</p>
+<center><button type="button" style="background-color: ##008CBA; border: none; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;"><a href="/perp">Perpetuity</button></a>
+<p>&nbsp;</p>
+<center><button type="button" style="background-color: #f44336; border: none; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;"><a href="/mort">Monthly Mortgage Payments</button></a>
+""".strip()
+
+# Present Value of a Bond
+pv_bondHTML = """
+<!-- OMIS30 HTML Template-->
+<style>body{background-color: #ffb366;}</style>
+<p><br /><center><h1>Present Value of a Bond</h1></p>
+<p>&nbsp;</p>
+<p><center>Enter your parameters below, then click submit:</p>
+<form action="/pv_bond_result">
+
+<p>
+Interest Rate %:<br /> 
+<input name="irate" type="number" step="0.01" value="1" /> 
+<br /> 
+
+Compounding Frequency:
+<br /> 
+<input name="comp_fq" type="number" step="0.01" value="2" /> 
+
+<br />
+Coupon Pmt:
+<br /> 
+<input name="p_cf" type="number" step="0.01" value="10" /> 
+
+<br /> 
+Face (Future) Value:
+<br /> 
+<input name="f_v" type="number" step="0.01" value="1000" /> 
+
+<br /> 
+Number of Years:
+<br /> 
+<input name="n_y" type="number" step="0.01" value="1" /> 
+
+<br /><br /> 
+<input type="submit" value="Submit" />
+</p>
+
+<p>&nbsp;</p>
+<a href='/calc_home'>Choose a different calculator</a>
+
+</form>
+""".strip()
+
+# for pv_bond_resultHTML, page crashes if I try to put in the code for backgrond color
+pv_bond_resultHTML = """
+<!-- OMIS30 HTML Template-->
+<p>&nbsp;</p>
+<p><center>You entered:</p>
+<table>
+
+<thead>
+
+<tr nth-child(even)>
+<th padding: 15px; text-align: left; border-bottom: 1px solid #ddd; background-color: #4CAF50; color: white;><center>Parameters</th>
+<th padding: 15px; text-align: left; border-bottom: 1px solid #ddd; background-color: #4CAF50; color: white;><center>Value</th>
+</tr>
+</thead>
+
+
+<tbody>
+
+<tr>
+<td><center>Interest Rate %</td>
+<td><center>{irate_field}</td>
+</tr>
+
+<tr>
+<td><center>Compounding Frequency</td>
+<td><center>{comp_fq_field}</td>
+</tr>
+
+<tr>
+<td><center>Coupon Pmt</td>
+<td><center>{p_cf_field}</td>
+</tr>
+
+<tr>
+<td><center>Face (Future) Value</td>
+<td><center>{f_v_field}</td>
+</tr>
+
+<tr>
+<td><center>Number of Years</td>
+<td><center>{n_y_field}</td>
+</tr>
+
+</tbody>
+</table>
+
+<p><h1><center>Present Value of your bond is <span style="background-color: #FFFF00">{pv_field}</span></h1></p>
+
+<p>&nbsp;</p>
+<p><img src="https://i.postimg.cc/FHX9kKM7/home-icon.png" vertical-align="top" style="width:30px;height:30px;">&nbsp;&nbsp;&nbsp;<a href='/'>Go to home page</a></p>
+<p>&nbsp;</p>
+<img src="https://i.postimg.cc/65xHD96K/imageedit-2-6626502388.png" vertical-align="top" style="width:30px;height:40px;">&nbsp;&nbsp;&nbsp;<a href='/calc_home'>Do another calculation</a>
+""".strip()
+
+# Perpetuity
+perpHTML = """
+<!-- OMIS30 HTML Template-->
+<style>body{background-color: #ffb366;}</style>
+<p><br /><center><h1>Perpetuity</h1></p>
+<p>&nbsp;</p>
+<p><center>Enter your parameters below, then click submit:</p>
+<form action="/perp_result">
+
+<p>
+Interest Rate %:<br /> 
+<input name="irate" type="number" step="0.01" value="1" /> 
+<br /> 
+
+<br />
+Coupon Pmt:
+<br /> 
+<input name="p_cf" type="number" step="0.01" value="10" /> 
+
+<br /><br /> 
+<input type="submit" value="Submit" />
+</p>
+
+<p>&nbsp;</p>
+<a href='/calc_home'>Choose a different calculator</a>
+
+</form>
+""".strip()
+
+# for perp_resultHTML, page crashes if I try to put in the code for backgrond color
+perp_resultHTML = """
+<!-- OMIS30 HTML Template-->
+<p>&nbsp;</p>
+<p><center>You entered:</p>
+<table>
+<tbody>
+
+<th padding: 15px; text-align: left; border-bottom: 1px solid #ddd; background-color: #4CAF50; color: white;><center>Parameters</th>
+<th padding: 15px; text-align: left; border-bottom: 1px solid #ddd; background-color: #4CAF50; color: white;><center>Value</th>
+
+<tr>
+<td><center>Interest Rate %</td>
+<td><center>{irate_field}</td>
+</tr>
+
+<tr>
+<td><center>Coupon Pmt</td>
+<td><center>{p_cf_field}</td>
+</tr>
+
+</tbody>
+</table>
+
+<p><h1><center>The value of your perpetuity is <span style="background-color: #FFFF00">{pv_field}</span></h1></p>
+
+<p>&nbsp;</p>
+<p><img src="https://i.postimg.cc/FHX9kKM7/home-icon.png" vertical-align="top" style="width:30px;height:30px;">&nbsp;&nbsp;&nbsp;<a href='/'>Go to home page</a></p>
+<p>&nbsp;</p>
+<img src="https://i.postimg.cc/65xHD96K/imageedit-2-6626502388.png" vertical-align="top" style="width:30px;height:40px;">&nbsp;&nbsp;&nbsp;<a href='/calc_home'>Do another calculation</a>
+""".strip()
+
+# Monthly Mortgage Payments
+mortHTML = """
+<!-- OMIS30 HTML Template-->
+<style>body{background-color: #ffb366;}</style>
+<p><br /><center><h1>Monthly Mortgage Payments</h1></p>
+<p>&nbsp;</p>
+<p><center>Enter your parameters below, then click submit:</p>
+<form action="/mort_result">
+
+<p>
+Loan Amt:
+<br /> 
+<input name="loan" type="number" step="0.01" value="1000" /> 
+<br/>
+
+Yearly Interest %:<br /> 
+<input name="irate" type="number" step="0.01" value="1" /> 
+<br />
+
+Years:<br /> 
+<input name="loan_term" type="number" step="0.01" value="30" /> 
+<br />
+
+
+<br /><br /> 
+<input type="submit" value="Submit" />
+</p>
+
+<p>&nbsp;</p>
+<a href='/calc_home'>Choose a different calculator</a>
+
+</form>
+""".strip()
+
+# for mortresultHTML, page crashes if I try to put in the code for background color
+mort_resultHTML = """
+<!-- OMIS30 HTML Template-->
+<p>&nbsp;</p>
+<p><center>The user entered:</p>
+<table>
+<tbody>
+
+<tr>
+<td><center>Loan Amt</td>
+<td><center>{loan_field}</td>
+</tr>
+
+<tr>
+<td><center>Yearly Interest %</td>
+<td><center>{irate_field}</td>
+</tr>
+
+<tr>
+<td><center>Years</td>
+<td><center>{loan_term_field}</td>
+</tr>
+
+</tbody>
+</table>
+
+<p><h1><center>Your monthly payment amount is <span style="background-color: #FFFF00">{mo_pmt_field}</span></h1></p>
+
+<p>&nbsp;</p>
+<p><img src="https://i.postimg.cc/FHX9kKM7/home-icon.png" vertical-align="top" style="width:30px;height:30px;">&nbsp;&nbsp;&nbsp;<a href='/'>Go to home page</a></p>
+<p>&nbsp;</p>
+<img src="https://i.postimg.cc/65xHD96K/imageedit-2-6626502388.png" vertical-align="top" style="width:30px;height:40px;">&nbsp;&nbsp;&nbsp;<a href='/calc_home'>Do another calculation</a>
+""".strip()
+
+errorMessageHTML = """
+<p>{error_field}</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<a href='/calc_home'>Try Again</a>
+""".strip()
